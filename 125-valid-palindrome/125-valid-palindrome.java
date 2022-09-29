23dc9d242ccdbf39920b84c88c7e19d9ecf59844
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-09-29 12:30:46 +0000

    Time: 27 ms (43.01%), Space: 64.4 MB (14.59%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -4,21 +4,18 @@ class Solution {
         
         if (s.length() == 0 || s.length() == 1) return true;
         
-        ArrayList<Character> sArray = new ArrayList<>();
-        ArrayList<Character> revArray = new ArrayList<>();
-        
-        
-        for (char c : s.toCharArray()) {
-            if (Character.isLetter(c) || Character.isDigit(c)) {
-                char l = Character.toLowerCase(c);
-                sArray.add(l);
-                revArray.add(0, l);
-            }
-        }
-        // if (revArray.size() == 1) return false;
-        // else{
-        //     return sArray.equals(revArray);
-        // }
-        return sArray.equals(revArray);
+        return checkPalindrome(s.toLowerCase(), 0, s.length() - 1);
+    }
+    
+    public boolean checkPalindrome(String s, int l, int r) {
+        if (s == null || l < 0 || r < 0) return true;
+        if (l >= r) return true;
+        if (!(Character.isLetter(s.charAt(l)) || Character.isDigit(s.charAt(l))))
+            return checkPalindrome(s, l + 1, r);
+        if (!(Character.isLetter(s.charAt(r)) || Character.isDigit(s.charAt(r)))) {
+            return checkPalindrome(s, l, r - 1);}
+        if (s.charAt(l) == s.charAt(r))
+            return checkPalindrome(s, l + 1, r - 1);
+        return false;
     }
 }
