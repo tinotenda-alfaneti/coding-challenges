c8ab66c240e314623f1bf221267239ecd0852cc9
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-11-09 23:54:13 +0000

    Time: 0 ms (100.00%), Space: 41.3 MB (66.20%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int rob1 = 0, rob2 = 0;
+
+        for(int num : nums) {
+            int temp = Math.max(rob1 + num, rob2);
+            rob1 = rob2;
+            rob2 = temp; 
+        }
+
+        return rob2;
+        
+    }
+}
