17d0ac85249be46fa7d21ea09a40c4bc6acbd4d1
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-11-10 00:10:58 +0000

    Time: 1 ms (24.02%), Space: 41.9 MB (15.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,28 @@
+/*
+Available option are basically 3, rob the current house and the house after the previous one
+or skip the current house and rob the previous house.
+
+Get the maximum of those actions and return that.
+
+*/
+
 class Solution {
-    public int rob(int[] nums) {
-
-        int rob1 = 0, rob2 = 0;
-
-        for(int num : nums) {
-            int temp = Math.max(rob1 + num, rob2);
-            rob1 = rob2;
-            rob2 = temp; 
-        }
-
-        return rob2;
+    public int rob(int[] nums) { 
         
+        int res = recurseRob(nums, nums.length - 1, new HashMap<Integer, Integer>());
+        return res;
+        
+    }
+    
+    public int recurseRob(int[] nums, int i, HashMap<Integer, Integer> memo) {
+        if (i < 0)
+            return 0;
+        
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+        int ans = Math.max(recurseRob(nums, i - 2, memo) + nums[i], recurseRob(nums, i - 1, memo));
+        memo.put(i, ans);
+        return memo.get(i);
     }
 }
