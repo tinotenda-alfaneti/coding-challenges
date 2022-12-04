df187606de504c6e0321bddc4c85fc829c737b52
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-12-04 13:15:58 +0000

    Time: 25 ms (63.54%), Space: 74.5 MB (70.74%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -5,37 +5,46 @@ Do the same thing coming from the right an store in a different array
 
 After that loop through the first array
 Take the element at the corresponding index and divided by length minus current index
+
+It turns out we can optimize on the space by getting rid of the prefix and sufix array so that we can only have a variable currPrefix, totalSum 
+The total sum - currPrefix gives us the right sum
+That's all we need
 */
 class Solution {
     public int minimumAverageDifference(int[] nums) {
-        int len = nums.length;
-        long[] leftPass = new long[len];
-        long[] rightPass = new long[len];
-        if (len == 1) return 0;
+        int n = nums.length;
+        int ans = -1;
+        int minAvgDiff = Integer.MAX_VALUE;
+        long currPrefixSum = 0;
         
-        leftPass[0] = nums[0];
-        for(int i = 1; i < len; i++) {
-            long res = leftPass[i - 1] + nums[i];
-            leftPass[i] = res;
-        }
-      
-        rightPass[len - 1] = 0;
-        for(int i = len - 1; i > 0; i--) {
-            long res = rightPass[i] + nums[i];
-            rightPass[i - 1] = res;
+        // Get total sum of array.
+        long totalSum = 0;
+        for (int index = 0; index < n; ++index) {
+            totalSum += nums[index];
         }
         
-        long minSoFar = Integer.MAX_VALUE;
-        int minIndex = -1;
-        
-        for (int i = 0; i < len; i++) {
+        for (int index = 0; index < n; ++index) {
+            currPrefixSum += nums[index];
             
-            long avgDiff = len - (i + 1) != 0 ? Math.abs((leftPass[i]/(i+1)) - (rightPass[i]/(len - (i + 1)))) : (leftPass[i]/(i+1));
-            if (minSoFar > avgDiff) {
-                minSoFar = avgDiff;
-                minIndex = i;
+            // Calculate average of left part of array, index 0 to i.
+            long leftPartAverage = currPrefixSum / (index + 1);
+            
+            // Calculate average of right part of array, index i+1 to n-1.
+            long rightPartAverage = 0;
+            // If right part have 0 elements, then we don't divide by 0.
+            if (index != n - 1) {
+                rightPartAverage = (totalSum - currPrefixSum) / (n - index - 1);
+            }
+            
+            int currDifference = (int) Math.abs(leftPartAverage - rightPartAverage);
+            // If current difference of averages is smaller,
+            // then current index can be our answer.
+            if (currDifference < minAvgDiff) {
+                minAvgDiff = currDifference;
+                ans = index;
             }
         }
-        return minIndex;
+        
+        return ans;
     }
 }
