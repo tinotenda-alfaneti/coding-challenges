834f592929c47aaf5e6dd7084a75a905c7576235
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2023-06-02 00:01:00 +0000

    Time: 0 ms (100.00%), Space: 41.6 MB (83.20%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -9,18 +9,8 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
         int lastIdx = m + n - 1;
-        
         int n1 = m-1;
         int n2 = n-1;
-        
-        if (n1 < 0 && n2 > -1) {
-            for (int i = 0; i <= n2; i++) {
-                nums1[i] = nums2[i];
-            }
-                
-            return;
-            }
-            
         
         for (int i = lastIdx; i >= 0; i--) {
             
