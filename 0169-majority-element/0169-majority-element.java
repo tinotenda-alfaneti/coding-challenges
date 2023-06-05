c04989787b0a355577eb0225b8eb101e12d7338a
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2023-06-05 00:18:19 +0000

    Time: 1 ms (99.63%), Space: 49 MB (5.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,19 +2,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        int count = 1, majElem = nums[0];
         
-        int majCount = nums.length / 2;
         
-        for(int i = 0; i < nums.length; i++) {     
-            counts.put(nums[i], counts.getOrDefault(nums[i], 0) + 1); 
+        for(int i = 1; i < nums.length; i++) {
+            
+            if (nums[i] == majElem) {
+                count++;
+            } else if (nums[i] != majElem && count != 0) {
+                count--;
+            } else if (nums[i] != majElem && count == 0) {
+                majElem = nums[i];
+                count = 1;
+            }
         }
         
-        for (int key : counts.keySet()) {
-            if (counts.get(key) > majCount) return key;
-        }
-        
-        return 0;  
+        return majElem;  
         
     }
 }
