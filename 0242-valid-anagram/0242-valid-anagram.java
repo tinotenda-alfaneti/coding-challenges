270ae610cc6df0070b70849896a6d94da13c8796
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2023-06-14 19:33:21 +0000

    Time: 19 ms (22.42%), Space: 44.1 MB (8.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() > t.length()) return false;
+        
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        }
+        
+        for (char c : t.toCharArray()) {
+            if (sMap.containsKey(c) && sMap.get(c) != 0) {
+                sMap.put(c, sMap.get(c) - 1);
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
