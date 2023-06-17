c775438476906880fd08a22bbd8da467e41c9104
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2023-06-17 23:58:30 +0000

    Time: 1 ms (99.97%), Space: 40.8 MB (77.27%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,21 +1,17 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         
-        HashMap<Character, Integer> sMap = new HashMap<>();
+        char res = 0;
         
         for (char c : s.toCharArray()) {
-            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+            res ^= c;
         }
         
         for (char c : t.toCharArray()) {
-            if (!sMap.containsKey(c) || sMap.get(c) == 0) {
-                return c;
-            } else if (sMap.containsKey(c)) {
-                sMap.put(c, sMap.get(c) - 1);
-            }
+            res ^= c;
         }
         
-        return '-';
+        return res;
         
     }
 }
