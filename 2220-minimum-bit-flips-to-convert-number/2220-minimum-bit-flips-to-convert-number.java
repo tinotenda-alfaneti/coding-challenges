faaf2df1321a34f3062e8c61eae44de1c92b5d4d
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2023-06-20 16:35:45 +0000

    Time: 0 ms (100.00%), Space: 39.5 MB (64.69%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        
+        int value = start^goal , count=0;
+        while(value > 0)
+        {
+            value &= value-1;
+            count++;
+        }
+        return count;
+        
+    }
+}
