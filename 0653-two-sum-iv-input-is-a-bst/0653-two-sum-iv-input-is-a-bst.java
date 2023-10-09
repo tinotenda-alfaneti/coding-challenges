96d4e306cbf8a93b771c628c3295d0c5a5570c94
Tinotenda Rodney Alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2023-10-09 15:38:42 +0000

    Time: 2 ms (95.93%), Space: 43.6 MB (75.39%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,37 +14,17 @@
  * }
  */
 class Solution {
-    HashMap<Integer, TreeNode> diffMap = new HashMap<>();
-    HashMap<Integer, TreeNode> nodesMap = new HashMap<>();
+    HashSet<Integer> seenSet = new HashSet<>();
+    
     public boolean findTarget(TreeNode root, int k) {
         
-        traverseTree(root, k);
+        if (root == null) return false;
         
-        for (int num : diffMap.keySet()) {
-            
-            if (nodesMap.containsKey(num)) {
-                TreeNode diffNode = diffMap.get(num);
-                TreeNode orgNode = nodesMap.get(num);
-                if (diffNode != orgNode) return true;
-            }
-        }
+        if (seenSet.contains(k - root.val)) return true;
         
-        return false;
+        seenSet.add(root.val);
         
+        return (findTarget(root.left, k) || findTarget(root.right, k));
     }
-    
-    public void traverseTree(TreeNode root, int target) {
         
-        if (root == null) return;
-        diffMap.put(target - root.val, root);
-        nodesMap.put(root.val, root);
-        
-        if (root.left != null) {
-            traverseTree(root.left, target);
-        }
-        if (root.right != null) {
-            traverseTree(root.right, target);
-        }
-        
-    }
 }
